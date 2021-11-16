a40fafc88555f8d5be221ae57a09d5a1c5db58d7
Maria Manoela <manoelamvc@hotmail.com>
2021-11-16 17:36:11 -0300

    :tada: First commit

---
+++ src/main/java/SpringDesenvolveTechApplication.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDesenvolveTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDesenvolveTechApplication.class, args);
+	}
+}
---
+++ src/test/java/webServices/SpringDesenvolveTechApplicationTests.java
@@ -0,0 +1,13 @@
+package webServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDesenvolveTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
