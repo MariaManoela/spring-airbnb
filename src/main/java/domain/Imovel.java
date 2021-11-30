545ab0febbcb1f7c805c37061613fa30f3133b16
Maria Manoela <manoelamvc@hotmail.com>
2021-11-30 14:58:07 -0300

    Adding lombok and entity

--- src/main/java/domain/Imovel.java
+++ src/main/java/domain/Imovel.java
@@ -1,6 +1,20 @@
 package domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
 public class Imovel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String nomeImovel;
     private boolean disponibilidade;
     private String cidade;
@@ -8,51 +22,7 @@ public class Imovel {
     private float precoDiaria;
     private String locatario;
 
-    public String getNomeImovel() {
-        return nomeImovel;
-    }
-
-    public void setNomeImovel(String nomeImovel) {
-        this.nomeImovel = nomeImovel;
-    }
-
     public boolean isDisponibilidade() {
         return disponibilidade;
-    }
-
-    public void setDisponibilidade(boolean disponibilidade) {
-        this.disponibilidade = disponibilidade;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public TipoImovel getTipoImovel() {
-        return tipoImovel;
-    }
-
-    public void setTipoImovel(TipoImovel tipoImovel) {
-        this.tipoImovel = tipoImovel;
-    }
-
-    public float getPrecoDiaria() {
-        return precoDiaria;
-    }
-
-    public void setPrecoDiaria(float precoDiaria) {
-        this.precoDiaria = precoDiaria;
-    }
-
-    public String getLocatario() {
-        return locatario;
-    }
-
-    public void setLocatario(String locatario) {
-        this.locatario = locatario;
     }
 }
--- src/main/java/domain/Usuario.java
+++ src/main/java/domain/Usuario.java
@@ -1,36 +1,28 @@
 package domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String user;
     private String senha;
     private TipoUsuario tipoUsuario;
 
+    public Usuario() {}
+
     public Usuario(String user, String senha, TipoUsuario tipoUsuario) {
         this.user = user;
         this.senha = senha;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public TipoUsuario getTipoUsuario(){
-        return tipoUsuario;
-    }
-
-    public void setTipoUsuario(TipoUsuario tipoUsuario){
-        this.tipoUsuario = tipoUsuario;
     }
 }
